e821a6fc404f3aecef9d838bd36d01502ca9b191
frabbit <frabbit77@googlemail.com>
2013-01-27 20:53:43 +0100

    settings renamed

--- haxe/settings.py
+++ haxe/settings.py
@@ -52,41 +52,44 @@ def get_string (id, default, view = None):
 			return r
 		else :
 			return None
+
+
+
 		
-	
+
 def no_fuzzy_completion (view = None):
-	return get_bool("no-fuzzy-completion", False, view)
+	return get_bool("haxe_completion_no_fuzzy", False, view)
 
 def top_level_completions_on_demand (view = None):
-	return get_bool("top-level-completions-on-demand", False, view)
+	return get_bool("haxe_completions_top_level_on_demand", False, view)
 
 def only_delayed_completions (view = None):
-	return get_bool("only-delayed-completions", False, view)
+	return get_bool("haxe_completions_only_delayed", False, view)
 
 def is_delayed_completion (view = None):
-	return get_bool("delayed-completion", False, view)
+	return get_bool("haxe_completion_delayed", False, view)
 
 def get_completion_delays (view = None):
 	return (
-		get_int("delayed-completion-timing-hide", 50, view),
-		get_int("delayed-completion-timing-show", 170, view)
+		get_int("haxe_completion_delayed_timing_hide", 50, view),
+		get_int("haxe_completion_delayed_timing_show", 170, view)
 	)
 
 
 def show_completion_times (view = None):
-	return get_bool("show-completion-times", False, view)
+	return get_bool("haxe_completion_show_times", False, view)
 
 
 def haxe_exec (view = None):
-	return get_string("haxe-exec", "haxe", view)
+	return get_string("haxe_exec", "haxe", view)
 
 def haxe_library_path (view = None):
-	res = get_string("haxe-library-path", None, view)
+	res = get_string("haxe_library_path", None, view)
 	return res
 	
 
 def haxelib_exec (view = None):
-	return get_string("haxelib-exec", "haxelib", view)
+	return get_string("haxe_haxelib_exec", "haxelib", view)
 	
 def smart_snippets (view = None):
-	return get_string("smart-snippets", "only", view)	
+	return get_string("haxe_completion_smart_snippets", "only", view)	
