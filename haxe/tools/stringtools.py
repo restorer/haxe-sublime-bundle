91ad72dffc8e88697c7daed87b8a8ac35f0a2fd1
frabbit <frabbit77@googlemail.com>
2013-06-16 14:31:18 +0200

    minor fix

--- haxe/tools/stringtools.py
+++ haxe/tools/stringtools.py
@@ -1,5 +1,8 @@
-def startswith_any (st, list_str):
-	for s in list_str:
-		if st.startswith(st):
+def startswith_any (s, list_str):
+	for s1 in list_str:
+		if s.startswith(s1):
 			return True
 	return False
+
+def reverse (s):
+	return s[::-1]
