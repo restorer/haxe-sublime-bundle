04edaeda8972eee9f1df054d1e3d19d8c24e52b4
clemos <cl3mos@gmail.com>
2013-06-24 12:49:39 +0200

    show doc : don't fail when no "doc" is returned

--- haxe/commands/show_doc.py
+++ haxe/commands/show_doc.py
@@ -15,7 +15,10 @@ class HaxeShowDocCommand( HaxeFindDeclarationCommand ):
 
 
     def handle_successfull_result(self, view, json_res, using_insert, insert_before, insert_after, expr_end, build, temp_path, temp_file):
-        doc = json_res["doc"]
+        if "doc" in json_res :
+        	doc = json_res["doc"]
+        else :
+        	doc = "No documentation found"
         #log("json: " + str(json_res))
         #log("doc: " + str(doc))
         panel.slide_panel().writeln(msg=doc, show_timestamp=False)
